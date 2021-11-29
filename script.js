26a8a6e9e26033e54b915e3dd02fb1f4ff462687
Esmaquel <88023702+Esmaquel@users.noreply.github.com>
2021-11-29 23:25:13 +1300

    map and carousel

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById('map'),{
             zoom: 8,
             center: wellington,
-    
+
         });
 
     const marker = new google.maps.Marker({
         position: wellington,
         map:map,
-           
+
     });
 };
 let objectArray = [
@@ -518,6 +518,7 @@ let objectArray = [
         Facilities2: '',
         Facilities3: '',
         Facilities4: '',
+        Facilities5: '',
         meal:['breakfast', 'lunch', 'dinner', 'kids meal'],
         minAdultGuests: 1,
         maxAdultGuests:4,
@@ -775,21 +776,34 @@ $(".moreInformation").click(function(){
                      <i class="fas fa-wifi" ${objectArray[i].Facilities2}></i>
                      <i class="fas fa-sink" ${objectArray[i].Facilities3}></i>
                      <i class="fab fa-accessible-icon" ${objectArray[i].Facilities4}></i>
+                     <i class="fas fa-tv" ${objectArray[i].Facilities5}></i>
 
 
-                     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="${objectArray[i].image01}" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                            <img src="${objectArray[i].image02}" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
+                            <div class="carousel-item active" data-interval="10000">
                             <img src="${objectArray[i].image03}" class="d-block w-100" alt="...">
                             </div>
+                            <div class="carousel-item" data-interval="2000">
+                            <img src="${objectArray[i].image01}" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                            <img src="${objectArray[i].image02}" d-block w-100" alt="...">
+                            </div>
                         </div>
+                        <button class="carousel-control-prev" type="button" data-target="#carouselExampleInterval" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-target="#carouselExampleInterval" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </button>
                         </div>
+
+
+
+
 
 
                     `
